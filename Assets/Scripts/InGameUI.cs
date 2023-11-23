726d8f635b3deca236c1d2207161e5bd27e77765
mercedes sandu <mxrcedes@gmail.com>
2023-11-22 21:55:23 -0600

    wip ui

--- Assets/Scripts/InGameUI.cs
+++ Assets/Scripts/InGameUI.cs
@@ -1,6 +1,26 @@
+using TMPro;
 using UnityEngine;
 
 public class InGameUI : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI turnText;
+
+    private bool _isPlayerTurn = true;
     
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Start()
+    {
+        GameEvent.OnTurnComplete += UpdateTurnText;
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    private void UpdateTurnText()
+    {
+        _isPlayerTurn = !_isPlayerTurn;
+        turnText.text = _isPlayerTurn ? "Player's Turn" : "Opponent's Turn";
+    }
 }
